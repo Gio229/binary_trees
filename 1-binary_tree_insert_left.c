0e435abac685b592dcbf3a7bf5f1cdb23ce0c882
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2022-09-25 12:13:30 +0100

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	newNode = malloc(sizeof(binary_tree_t));
 
-	if (newNode == NULL || parrent == NULL)
+	if (newNode == NULL || parent == NULL)
 		return (NULL);
 
 	newNode->n = value;

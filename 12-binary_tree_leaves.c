b9d4f854ba1fc7c1282410e4c4aabf0ead75e4d3
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-09-27 10:45:11 +0100

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,11 +10,18 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leavesLeft, leavesRight;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->right  == NULL && tree->left == NULL)
-		livesLeft = binary_tree_leaves(tree->left);
-		livesRight = binary_tree_leaves(tree->right)
-		return (1 + leavesLeft  + livesRight);
+	{
+		return (1);
+	}
+
+	leavesLeft = binary_tree_leaves(tree->left);
+	leavesRight = binary_tree_leaves(tree->right);
+
+	return (leavesLeft  + leavesRight);
 }

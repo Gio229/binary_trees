4e114338d606c122b797e7aeae0541ddf9f8d5c8
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-09-27 09:38:10 +0100

    taskj 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,14 +4,14 @@
 
 
 /**
- * size_t binary_tree_depth - 
+ * binary_tree_depth - measures the depth of a node
  * @tree: measures the depth of a node in a binary tree
- * Return the depth
+ * Return: return the depth
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL || tree->parent)
 		return (0);
-	
+
 	return (binary_tree_depth(tree->parent) + 1);
 }

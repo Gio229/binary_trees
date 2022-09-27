78454adc2bc9ce79be3ac6fbe544e5a564e36b0e
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-09-27 09:53:33 +0100

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -15,6 +15,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	rightSize = binary_tree_size(tree->right) + 1;
 	leftSize = binary_tree_size(tree->left)  +  1;
-	return ( rightSize + leftSize);
+	return (rightSize + leftSize);
 
 }
